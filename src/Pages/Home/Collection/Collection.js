568d5071022041d5bd776a02ef555e62e3c91c2a
ImtiazMahmod <imtiazmahmod2@gmail.com>
2021-12-06 13:58:10 +0600

    spinner add

--- src/Pages/Home/Collection/Collection.js
+++ src/Pages/Home/Collection/Collection.js
@@ -2,16 +2,19 @@ import { Container, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Spinner from "../../Shared/Spinner/Spinner";
 import SingleBike from "./SingleBike/SingleBike";
 
 const Collection = () => {
   const [bikes, setBikes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   //load all bikes
   useEffect(() => {
     axios
       .get("https://nameless-fortress-10028.herokuapp.com/bikes")
       .then((res) => {
         setBikes(res.data);
+        setIsLoading(false);
       });
   }, []);
   return (
@@ -21,16 +24,20 @@ const Collection = () => {
         {" "}
         BIKE RANGE
       </Typography>
-      <Container>
-        <Grid container spacing={3} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {/* show only 6 bikes */}
-          {bikes?.slice(0, 6).map((bike) => (
-            <Grid key={bike._id} item xs={4} sm={4} md={4}>
-              <SingleBike bike={bike} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Container>
+          <Grid container spacing={3} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {/* show only 6 bikes */}
+            {bikes?.slice(0, 6).map((bike) => (
+              <Grid key={bike._id} item xs={4} sm={4} md={4}>
+                <SingleBike bike={bike} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      )}
     </Box>
   );
 };
